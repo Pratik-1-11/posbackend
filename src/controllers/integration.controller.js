d4aebc6b0f34ba57e515c0d2aaf4be4c19abbf65
Pratik-1-11 <pratikdevkota05@gmail.com>
2026-03-13 14:35:10 +0545

    fix: correct named import of supabase client in integration controller

--- src/controllers/integration.controller.js
+++ src/controllers/integration.controller.js
@@ -1,4 +1,4 @@
-import { supabase } from '../config/supabase.js';
+import supabase from '../config/supabase.js';
 import { StatusCodes } from 'http-status-codes';
 import logger from '../utils/logger.js';
 
